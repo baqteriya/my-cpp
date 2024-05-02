12960e639fe47184841cb41d3cee05be607cac30
baqteriya <kumaririya1030@gmail.com>
2024-05-02 21:06:17 +0530

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,25 +1,33 @@
 #include<stdio.h>
-#include<stdlib.h>
+
 int main()
 {
-int i=0;
-int item,a[5]={34,56,32,78,12};
-printf("enter the searching item");
-scanf("%d",&item);
-while(i<5)
-{
-if(a[i]==item)
-{
-printf("item found of%d",i);
-exit (0);
+    int a[5]={10,20,30,40,50};
+    int ir=0,up=4,mid,item,f=0;
+    printf("enter the searching item:");
+    scanf("%d",&item);
+    while(ir<=up)
+    {
+        mid=(ir+up)/2;
+        if(a[mid]==item)
+        {
+            f=1;break;
+        }
+        if(a[mid]<item)
+        {
+            ir=mid+1;
+        }
+        else
+ {
+     up=mid-1;
+ }
+ if(f==1)
+ {
+     printf("item found at%d",mid);
+ }
+ else
+    {
+        printf("item not found");
+    }
 }
-i++;
 }
-if(i>=5)
-{
-printf("item not found");
-exit (0);
-}
-
-}
-
